8d60428b4880bdbb90900c35fe9832695471a0a6
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2024-12-25 17:53:14 -0300

    Ajustando o projeto no JS

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 include 'queries/buscarUnidades.php';  // Consulta as unidades
 
+$section = isset($_GET['section']) ? $_GET['section'] : 'criarAgenda';
+
+switch ($section) {
+    case 'criarAgenda':
+        $page = 'sections/criarAgenda.php';
+        break;
+    case 'agendarAtendimento':
+        $page = 'sections/agendarAtendimento.php';
+        break;
+    case 'faltasConsecutivas':
+        $page = 'sections/faltasConsecutivas.php';
+        break;
+    default:
+        $page = 'sections/criarAgenda.php';
+        break;
+}
+
 // Consulta para buscar os nomes das unidades
 // $sql = "SELECT * FROM rededadosgerais";
 // $stmt = $conn->prepare($sql);
@@ -57,126 +74,8 @@ include 'queries/buscarUnidades.php';  // Consulta as unidades
         </div>
 
         <div class="container">
-            <!-- Fluxo para Criar Agenda -->
-            <section id="criar-agenda">
-                <h2>Criar Agenda</h2>
-                <div>
-                    <label for="unidade">Selecione a Unidade:</label>
-                    <select id="unidade">
-                        <!-- <option value="selecione">Selecione...</option> -->
-                        <?php foreach ($unidades as $unidade): ?>
-                            <option value="<?= htmlspecialchars($unidade['cod']) ?>">
-                                <?= htmlspecialchars($unidade['nome']) ?>
-                            </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-
-                <div id="corpo-clinico" class="hidden">
-                    <table id="medicosTabela" class="display">
-                        <thead>
-                            <tr>
-                                <th>Cod</th>
-                                <th>Nome</th>
-                                <th>Especialidade</th>
-                                <th>Nº Conselho</th>
-                                <th>Ação</th>
-                            </tr>
-                        </thead>
-                        <tbody id="medicos-tabela">
-                            <!-- Médicos serão carregados aqui via AJAX -->
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div id="agenda-modal" class="modal">
-        <div class="modal-content">
-            <header>
-                <span class="close">&times;</span>
-                <h1></h1>
-            </header>
-            <form>
-                <div class="form-group">
-                    <label for="vigencia">Vigência:</label>
-                    <div class="form-div-group">
-                        <input type="date" id="vigencia-inicio">
-                        <span>até</span>
-                        <input type="date" id="vigencia-fim">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="dia-agendamento">Dia de Agendamento:</label>
-                    <div id="dias">
-                        <button type="button">SEG</button>
-                        <button type="button">TER</button>
-                        <button type="button">QUA</button>
-                        <button type="button">QUI</button>
-                        <button type="button">SEX</button>
-                        <button type="button">SAB</button>
-                    </div>
-                </div>
-
-                <div class="form-envelop">
-                    <div class="form-group">
-                        <label for="tipo">Tipo:</label>
-                        <select id="tipo">
-                            <option value="1">Ordem de chegada</option>
-                            <option value="2">Hora marcada</option>
-                            <option value="3">Hora prevista</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="contratacao">Tipo de Contratação:</label>
-                        <select id="contratacao">
-                            <option value="1">Plantão</option>
-                            <option value="2">Produção</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="beneficiarios">Quantidade de Beneficiários:</label>
-                        <input type="text" id="beneficiarios">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="especialidade">Especialidade:</label>
-                        <select id="especialidade">
-                            <option>Escolha</option>
-                        </select>
-                    </div>
-
-                </div>
-
-                <!-- Procedimentos -->
-                <div id="procedimentos-container">
-                    <div class="form-procedimento-select">
-                        <label for="procedimento">Procedimento:</label>
-                        <select class="form-select procedimento">
-                            <option value="">selecione...</option>
-                            <option value="1">EM CONSULTORIO (NO HORARIO NORMAL OU PREESTABELECIDO)</option>
-                            <option value="2">EM PRONTO SOCORRO</option>
-                            <option value="4163">CONSULTA EM PSICOLOGIA</option>
-                            <option value="4169">SESSAO DE PSICOTERAPIA INDIVIDUAL</option>
-                            <option value="4180">SESSAO DE PSICOPEDAGOGIA INDIVIDUAL</option>
-                            <option value="4187">SESSAO DE PSICOMOTRICIDADE INDIVIDUAL</option>
-                            <option value="4168">CONSULTA EM TERAPIA OCUPACIONAL</option>
-                            <option value="4195">SESSAO INDIVIDUAL AMBULATORIAL, EM TERAPIA OCUPACIONAL</option>
-                            <option value="4171">SESSAO INDIVIDUAL AMBULATORIAL DE FONOAUDIOLOGIA</option>
-                        </select>
-                    </div>
-                </div>
-
-                <!-- Botões para adicionar/remover selects -->
-                <div id="div-button">
-                    <button type="button" id="addProcedimento">Adicionar Procedimento</button>
-                    <button type="button" id="removeProcedimento">Remover Procedimento</button>
-                    <button type="submit" id="cadastrarAgenda">Cadastrar Agenda</button>
-                </div>
-            </form>
+            <!-- Fluxo das sections -->
+            <?php include $page; ?>
         </div>
     </div>
 
--- js/script.js
+++ js/script.js
@@ -34,8 +34,8 @@ $(document).ready(function () {
     function limparTabela() {
         if ($.fn.dataTable.isDataTable('#medicosTabela')) {
             table.destroy();
-            $('#medicosTabela tbody').empty();
         }
+        $('#medicosTabela tbody').empty();
     }
 
     function buscarMedicos(unidadeId) {
@@ -79,23 +79,27 @@ $(document).ready(function () {
             });
     }
 
+
+    // VALIDAÇÃO PARA EXIBIR O CORPO CLÍNICO DAS UNIDADES
     unidadeSelect.addEventListener("change", function () {
         const unidadeId = this.value;
 
-        if (unidadeId === "selecione") {
+        if (unidadeId == "selecione") {
+            // console.log(`A unidade selecionada é: ${unidadeId}`)
             corpoClinicoSection.classList.add("hidden");
             limparTabela();
         } else {
+            // console.log(`A unidade selecionada é: ${unidadeId}`)
             buscarMedicos(unidadeId);
         }
-    });
+    });    
 
-    // RETIREI/COMENTEI O SELECIONE DO INDEX
-    if (unidadeSelect.value === "selecione") {
-        corpoClinicoSection.classList.add("hidden");
-    } else {
-        buscarMedicos(unidadeSelect.value);
-    }
+    // // RETIREI/COMENTEI O SELECIONE DO INDEX
+    // if (unidadeSelect.value !== "selecione") {
+    //     buscarMedicos(unidadeSelect.value);
+    // } else {
+    //     corpoClinicoSection.classList.add("hidden");
+    // }
 
     // Modal e funções relacionadas
     const modal = document.getElementById('agenda-modal');
@@ -172,16 +176,12 @@ $(document).ready(function () {
 
     span.onclick = fecharModal;
 
-    // window.onclick = function (event) {
-    //     if (event.target === modal) {
-    //         fecharModal();
-    //     }
-    // }
-
+    // FUNÇÃO PARA SELECIONAR OS DIAS DA SEMANA
     document.querySelectorAll('#dias button').forEach(button => {
-        button.onclick = function () {
+        button.addEventListener('click', function () {
             this.classList.toggle('selected');
-        }
+            this.classList.toggle('active'); // Add both classes
+        });
     });
 
     document.addEventListener('click', function (e) {
@@ -204,163 +204,138 @@ $(document).ready(function () {
         }
     });
 
-    // ADD FUNCTION DOS BUTTONS ACTIVE
+    // Função de Validação dos campos para cadastrar agenda (MODAL)
+    document.querySelector('#div-button button[type="submit"]').addEventListener('click', function (e) {
+        e.preventDefault(); // Evita o envio do formulário diretamente
 
-    $(document).ready(function () {
-        // Inicializa variáveis
-        const buttons = document.querySelectorAll('.btn-option');
-        const sections = document.querySelectorAll('section');
+        // Seleção dos campos
+        const vigenciaInicio = document.getElementById('vigencia-inicio').value.trim();
+        const vigenciaFim = document.getElementById('vigencia-fim').value.trim();
+        const tipo = document.getElementById('tipo').value.trim();
+        const contratacao = document.getElementById('contratacao').value.trim();
+        const beneficiarios = document.getElementById('beneficiarios').value.trim();
+        const especialidade = document.getElementById('especialidade').value.trim();
 
-        // Função para ativar o botão e mostrar a seção
-        function ativarBotao(botaoClicado) {
-            // Remove a classe ativa de todos os botões e oculta todas as seções
-            buttons.forEach(btn => btn.classList.remove('active'));
-            sections.forEach(sec => sec.classList.add('hidden'));
+        // Captura a data atual
+        const dataAtual = new Date();
 
-            // Adiciona a classe ativa ao botão clicado e exibe a seção correspondente
-            botaoClicado.classList.add('active');
-            const sectionId = botaoClicado.textContent.trim().toLowerCase().replace(/\s+/g, '-');
-            const targetSection = document.getElementById(sectionId);
-            if (targetSection) {
-                targetSection.classList.remove('hidden');
-            }
+        // Zera a hora, minuto, segundo e milissegundo da data atual, mas usando UTC
+        dataAtual.setUTCHours(0, 0, 0, 0);
+
+        // Converte a data do campo para o formato Date
+        const vigenciaData = new Date(vigenciaInicio);
+
+        // Zera a hora, minuto, segundo e milissegundo da vigência, mas usando UTC
+        vigenciaData.setUTCHours(0, 0, 0, 0);
+
+        // Verifica se a data de vigência é maior ou igual à data atual
+        if (vigenciaData < dataAtual) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'A data de vigência não pode ser menor do que a data atual',
+            });
+            return;
         }
 
-        // Adiciona eventos de clique aos botões
-        buttons.forEach(button => {
-            button.addEventListener('click', function () {
-                ativarBotao(this);
+
+        // Verifica se ao menos um dia foi selecionado
+        const diasSelecionados = document.querySelectorAll('#dias button.selected').length > 0;
+
+        // Validação dos campos
+        if (!vigenciaInicio || !vigenciaFim || !tipo || !contratacao || !beneficiarios || !especialidade || !diasSelecionados) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Por favor, preencha todos os campos antes de cadastrar a agenda!',
             });
+            return;
+        }
+
+        // Exibe sucesso caso todos os campos estejam preenchidos
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Agenda cadastrada com sucesso!',
+            showConfirmButton: false,
+            timer: 3500
         });
 
-        // Define o botão "Criar Agenda" como ativo por padrão
-        const defaultButton = document.querySelector('.btn-option:nth-child(1)');
-        if (defaultButton) {
-            ativarBotao(defaultButton);
+        // Verificando se a quantidade de beneficiário é zero
+        if (beneficiarios == 0) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'A quantidade de beneficiário que serão atendidos não pode ser 0 (zero)!',
+            });
+            return;
         }
+
+        // Lógica para enviar os dados ao backend
     });
 
-    // ADICIONANDO ACTIVE PARA SELECIONAR OS DIAS DA SEMANA
+    // Limitação de selects para procedimentos
+    document.getElementById('add-button').addEventListener('click', function () {
+        const procedimentoSelects = document.querySelectorAll('.procedimento-select');
+        if (procedimentoSelects.length < 3) {
+            adicionarSelectProcedimento();
+        }
+    });
+    
+});
 
-    // Selecionar todos os botões dentro do elemento #dias
+    // ATIVANDO OS BOTÕES
+$(document).ready(function () {
+    // Inicializa variáveis
+    const buttons = document.querySelectorAll('.btn-option');
+    const sections = document.querySelectorAll('section');
+
+    // Função para ativar o botão e mostrar a seção correspondente
+    function ativarBotao(botaoClicado) {
+        // Remove a classe ativa de todos os botões e oculta todas as seções
+        buttons.forEach(btn => btn.classList.remove('active'));
+        sections.forEach(sec => sec.classList.add('hidden'));
+
+        // Adiciona a classe ativa ao botão clicado e exibe a seção correspondente
+        botaoClicado.classList.add('active');
+        const sectionId = botaoClicado.textContent.trim().toLowerCase().replace(/\s+/g, '-');
+        const targetSection = document.getElementById(sectionId);
+        if (targetSection) {
+            targetSection.classList.remove('hidden');
+        }
+    }
+
+    // Adiciona eventos de clique aos botões
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            ativarBotao(this);
+        });
+    });
+
+    // Define o botão "Criar Agenda" como ativo por padrão
+    const defaultButton = document.querySelector('.btn-option:nth-child(1)');
+    if (defaultButton) {
+        ativarBotao(defaultButton);
+    }
+
+    // Manter o comportamento de modal ativo
+    const modal = document.getElementById('agenda-modal');
+    const span = document.querySelector('.close');
+
+    span.onclick = function () {
+        modal.classList.remove('modal-active');
+        document.body.style.overflow = '';
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 300);
+    };
+
+    // Remover a classe 'active' nos botões de dia de forma similar
     const botoesDias = document.querySelectorAll('#dias button');
-
-    // Adicionar um evento de clique para cada botão
     botoesDias.forEach(botao => {
         botao.addEventListener('click', function () {
             // Alternar a classe 'active' ao clicar
             this.classList.toggle('active');
         });
     });
-});
-
-// Função de Validação dos campos para cadastrar agenda (MODAL)
-document.querySelector('#div-button button[type="submit"]').addEventListener('click', function (e) {
-    e.preventDefault(); // Evita o envio do formulário diretamente
-
-    // Seleção dos campos
-    const vigenciaInicio = document.getElementById('vigencia-inicio').value.trim();
-    const vigenciaFim = document.getElementById('vigencia-fim').value.trim();
-    const tipo = document.getElementById('tipo').value.trim();
-    const contratacao = document.getElementById('contratacao').value.trim();
-    const beneficiarios = document.getElementById('beneficiarios').value.trim();
-    const especialidade = document.getElementById('especialidade').value.trim();
-
-    // Captura a data atual
-    const dataAtual = new Date();
-
-    // Converte a data do campo para o formato Date
-    const vigenciaData = new Date(vigenciaInicio);
-
-    // Verifica se a data de vigência é maior ou igual à data atual
-    if (!vigenciaData >= dataAtual) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'A data de vigência não pode ser menor do que a data atual',
-        });
-        return;
-    }    
-
-    // Verifica se ao menos um dia foi selecionado
-    const diasSelecionados = document.querySelectorAll('#dias button.active').length > 0;
-
-    // Validação dos campos
-    if (!vigenciaInicio || !vigenciaFim || !tipo || !contratacao || !beneficiarios || !especialidade || !diasSelecionados) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Por favor, preencha todos os campos antes de cadastrar a agenda!',
-        });
-        return;
-    }
-
-    // Exibe sucesso caso todos os campos estejam preenchidos
-    Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Agenda cadastrada com sucesso!',
-        showConfirmButton: false,
-        timer: 3500
-    });
-
-    // Verificando se a quantidade de beneficiário é zero
-    if (beneficiarios == 0) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'A quantidade de beneficiário que serão atendidos não pode ser 0 (zero)!',
-        });
-        return;
-    }
-
-    // lógica para enviar os dados ao backend
-});
-
-// Adiciona um novo select para procedimento
-document.getElementById('addProcedimento').addEventListener('click', function() {
-    const container = document.getElementById('procedimentos-container');
-    
-    // Verifica quantos selects já existem, e limita a 3
-    const selects = container.getElementsByClassName('form-procedimento-select');
-    if (selects.length < 3) { // Limite de 3
-        // Cria um novo select
-        const novoSelect = document.createElement('div');
-        novoSelect.classList.add('form-procedimento-select');
-        
-        novoSelect.innerHTML = `        
-            <select class="form-select procedimento">
-                <option value="">selecione...</option>
-                <option value="1">EM CONSULTORIO (NO HORARIO NORMAL OU PREESTABELECIDO)</option>
-                <option value="2">EM PRONTO SOCORRO</option>
-                <option value="4163">CONSULTA EM PSICOLOGIA</option>
-                <option value="4169">SESSAO DE PSICOTERAPIA INDIVIDUAL</option>
-                <option value="4180">SESSAO DE PSICOPEDAGOGIA INDIVIDUAL</option>
-                <option value="4187">SESSAO DE PSICOMOTRICIDADE INDIVIDUAL</option>
-                <option value="4168">CONSULTA EM TERAPIA OCUPACIONAL</option>
-                <option value="4195">SESSAO INDIVIDUAL AMBULATORIAL, EM TERAPIA OCUPACIONAL</option>
-                <option value="4171">SESSAO INDIVIDUAL AMBULATORIAL DE FONOAUDIOLOGIA</option>
-            </select>
-        `;
-        
-        container.appendChild(novoSelect); // Adiciona o novo select ao container
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Você já atingiu o limite de 3 procedimentos.',
-        });
-        return;
-    }
-});
-
-// Remove o último select de procedimento
-document.getElementById('removeProcedimento').addEventListener('click', function() {
-    const container = document.getElementById('procedimentos-container');
-    
-    // Verifica se há mais de um select para remover
-    const selects = container.getElementsByClassName('form-procedimento-select');
-    if (selects.length > 1) {
-        container.removeChild(selects[selects.length - 1]); // Remove o último select
-    }
 });
